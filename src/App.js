54f0aeeec51fa499c8a5129204a8c2e4befd6c4e
Zahin Zawad <zahin.zawad.19@gmail.com>
2024-03-29 23:33:34 +0600

    first

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import "./App.css"
+import Todoinput from './components/Todoinput';
+import Todolist from './components/TodoList';
 
 function App() {
+
+  const [listTodo,setListTodo] = useState([]);
+
+  let addList = (inputText)=>{
+    if(inputText !== "")
+      setListTodo([...listTodo,inputText]);
+  }
+
+  const deleteListItem = (key) =>{
+    let newListTodo = [...listTodo];
+    newListTodo.splice(key,1);
+    setListTodo([...newListTodo]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main-container'>
+      <div className='center-container'>
+         <Todoinput addList = {addList} />
+         <h1 className='app-heading'>TODO</h1>
+         <hr/>
+         {listTodo.map((listItem,i)=>{
+          return(
+            <Todolist key = {i} index = {i} item = {listItem} deleteItem = {deleteListItem} />
+          )
+         })}
+      </div>
     </div>
   );
 }
